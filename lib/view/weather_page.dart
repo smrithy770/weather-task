e903f33f53d4682103ea007c1b5cc9bb50d86c68
smrithy770 <smrithygopinath999@gmail.com>
2024-01-09 16:35:41 +0530

    Current Weather

---
+++ lib/view/weather_page.dart
@@ -0,0 +1,134 @@
+import 'dart:developer';
+
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:weathercast/constants/constants.dart';
+import 'package:weathercast/logic/models/weather_model.dart';
+import 'package:weathercast/logic/services/call_to_api.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  Future<WeatherModel> getData(bool isCurrentCity, String cityName) async {
+    return await CallToApi().callWeatherAPi(isCurrentCity, cityName);
+  }
+
+  TextEditingController textController = TextEditingController(text: "");
+  Future<WeatherModel>? _myData;
+  @override
+  void initState() {
+    setState(() {
+      _myData = getData(true, "");
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: FutureBuilder(
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If error occured
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '${snapshot.error.toString()} occurred',
+                  style: TextStyle(fontSize: 18),
+                ),
+              );
+
+              // if data has no errors
+            } else if (snapshot.hasData) {
+              // Extracting data from snapshot object
+              final data = snapshot.data as WeatherModel;
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 65, 89, 224), // Replace with your desired color
+
+                ),
+                width: double.infinity,
+                height: double.infinity,
+                child: SafeArea(
+                  child: Column(
+                    children: [
+                      AnimSearchBar(
+                        rtl: true,
+                        width: 400,
+                        color: Colors.cyanAccent,
+                        textController: textController,
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                          size: 26,
+                        ),
+                        onSuffixTap: () async {
+                          textController.text == ""
+                              ? log("No city entered")
+                              : setState(() {
+                                  _myData = getData(false, textController.text);
+                                });
+
+                          FocusScope.of(context).unfocus();
+                          textController.clear();
+                        },
+                        style: f14RblackLetterSpacing2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                             "Today's Weather",
+                              style:f24Rwhitebold,
+                            ),
+                            SizedBox(height: 50,),
+                            Text(
+                              data.city,
+                              style: f24Rwhitebold,
+                            ),
+                            height25,
+                            Text(
+                              data.desc,
+                              style: f16PW,
+                            ),
+                            height25,
+                            Text(
+                              "${data.temp}°C",
+                              style: f42Rwhitebold,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Center(
+              child: Text("${snapshot.connectionState} occured"),
+            );
+          }
+          return Center(
+            child: Text("Server timed out!"),
+          );
+        },
+        future: _myData!,
+      ),
+    );
+  }
+}
